05089e7cec15ca87315b04e4c7b63f55bf7179a6
Tuan Anh Dinh <tuananh@Tuans-MBP.local>
2018-07-26 22:41:30 +0200

    chat ready

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require('socket.io')
 
 const app = express()
 const server = app.listen(4000, () => {
-	console.log('app on port 4000')
+    console.log('app on port 4000')
 })
 
 // static files
@@ -13,5 +13,9 @@ app.use(express.static('public'))
 const io = socket(server)
 
 io.on('connection', socket => {
-	console.log('socket connection made', socket.id)
+    console.log('socket connection made', socket.id)
+
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data)
+    })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,23 @@
 // make connection
 const socket = io.connect('http://localhost:4000')
+
+// DOM
+const $ = val => document.getElementById(val)
+
+let message = $('message')
+let handle = $('handle')
+let btn = $('send')
+let output = $('output')
+
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+})
+
+// listen for events
+socket.on('chat', data => {
+    output.innerHTML +=
+        '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+})
