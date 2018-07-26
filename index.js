edca59495ec73ae7d5ba17cb7a5265c70b991fc8
tudi <tadinh0691@gmail.com>
2018-07-26 15:41:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const socket = require('socket.io')
+
+const app = express()
+const server = app.listen(4000, () => {
+	console.log('app on port 4000')
+})
+
+// static files
+app.use(express.static('public'))
+
+// socket setup
+const io = socket(server)
+
+io.on('connection', socket => {
+	console.log('socket connection made', socket.id)
+})
